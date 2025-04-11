d243e07a02791b3bc4d60bb04753747017a6f5f4
Tisicd <letisidw@gmail.com>
2025-04-10 19:45:04 -0500

    Create app.py
    hola mundo

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+def  main()
+      root = tk.Tk()
+      root.title("  Programacion distribuida ")
+      root.geometry("300x100")
+
+      label = tk.label(root,text="Hola mundo", font=("Arial", 20)
+      label.pack(pady = 20)
+      root .mainloop()
+if __name__ = "__main__":
+      main()
